821964a7352d4036ba9104f3323190c0603f3015
Hanna Sarviro <hsarviro@immersetech.co>
2025-10-24 10:05:42 +0300

    fix update user phone number

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,32 +1,38 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, IsOptional, Matches, ValidateNested } from 'class-validator';
-import { Type } from 'class-transformer';
-import { SocialMediaDto } from './social-media.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import {
+  IsEmail,
+  IsString,
+  IsOptional,
+  Matches,
+  ValidateNested,
+} from "class-validator";
+import { Type } from "class-transformer";
+import { SocialMediaDto } from "./social-media.dto";
 
 export class UpdateUserDto {
-    @ApiProperty({ required: false })
-    @IsOptional()
-    @IsEmail()
-    email?: string;
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsEmail()
+  email?: string;
 
-    @ApiProperty({ required: false })
-    @IsOptional()
-    @IsString()
-    username?: string;
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  username?: string;
 
-    @ApiProperty({ required: false })
-    @IsOptional()
-    @IsString()
-    @Matches(/^$|^\+?[1-9]\d{1,14}$/, { message: 'Invalid phone number format' })
-    phone?: string;
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  @Matches(/^$|^\+?\d{10,14}$/, { message: "Invalid phone number format" })
+  phone?: string;
 
-    @ApiProperty({ 
-        required: false,
-        type: [SocialMediaDto],
-        description: 'Array of social media links'
-    })
-    @IsOptional()
-    @ValidateNested({ each: true })
-    @Type(() => SocialMediaDto)
-    socials?: SocialMediaDto[];
+  @ApiProperty({
+    required: false,
+    type: [SocialMediaDto],
+    description: "Array of social media links",
+  })
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => SocialMediaDto)
+  socials?: SocialMediaDto[];
 }
